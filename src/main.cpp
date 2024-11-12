27ec5c84a66c2be0f41d1138393ad1ea2fd807be
Harris Moustakas <harris.moustakas@ravenind.com>
2024-11-12 16:46:29 +0200

    Set the main demo

--- src/main.cpp
+++ src/main.cpp
@@ -1,29 +1,39 @@
+#include <AudioIO.hpp>
 #include <CudaDenoise.hpp>
-#include <FfmpegHandler.hpp>
+#include <ErrChecker.hpp>
 
+#include <cassert>
 #include <iostream>
 
 void printUsg() { std::cout << "./gpudenoiser /path/to/file.mp3 \n"; }
 
 void runDemo(const std::string_view &audio_file) {
 
-  gpudenoise::FfmpegHandler ffmpeg_handler(audio_file);
+  // Load as packet the .wav file
+  auto audio_packet = gpudenoise::loadAudioBufferFromWAV(audio_file);
+  assert(audio_packet.isValid());
+  gpudenoise::exportSignalToCSV("/tmp/clean_signal.csv", audio_packet);
 
-  const int cutoff_freq = 50;
-  float *audio_data = (float *)ffmpeg_handler.getAudioBuffer().data();
-  const size_t data_length = ffmpeg_handler.getAudioBuffer().size();
-  const int sample_rate = ffmpeg_handler.getSampleRate();
+  // Denoise in GPU the "noisy" signal for each channel, dependency injection is
+  // not possible due to the fact the nvcc and JUCE are somehow incompatible
+  const int num_of_samples = audio_packet.signal.getNumSamples();
+  const int num_of_channels = audio_packet.signal.getNumChannels();
+  const int sample_rate = audio_packet.sample_rate;
 
-  gpudenoise::gpuDenoiseData(audio_data, sample_rate, data_length);
+  for (int channel = 0; channel < num_of_channels; ++channel) {
+    float *audio_signal = audio_packet.signal.getWritePointer(channel);
+    gpudenoise::gpuDenoiseSignal(audio_signal, sample_rate, num_of_samples);
+  }
+
+  gpudenoise::exportSignalToCSV("/tmp/clean_signal.csv", audio_packet);
+
+  // Save as .wav file the clean signal
+  gpudenoise::saveWAVfromAudioBuffer("/tmp/clean.wav", audio_packet);
 }
 
 int main(int argc, char *argv[]) {
 
-  if (argc != 2) {
-    printUsg();
-    throw std::runtime_error("Invalid input to executable \n");
-  }
-
+  CHECK_THROW(argc == 2, "Invalid input to executable \n");
   std::string_view audio_file = argv[1];
   runDemo(audio_file);
 
